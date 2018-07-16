694f2c817e1ca3267de32d7ad5e623e35e41dede
XUWEIXUWEIXUWEI <1076623940@qq.com>
2018-07-16 20:32:16 +0800

    listview基本界面

--- app/src/main/java/com/example/administrator/androidwechat/SessionActivity.java
+++ app/src/main/java/com/example/administrator/androidwechat/SessionActivity.java
@@ -24,5 +24,13 @@ public class SessionActivity extends AppCompatActivity{
     private void initSession(){
         Session a=new Session(R.drawable.session_image,"xuwei","你好吗");
         sessionList.add(a);
+        Session b=new Session(R.drawable.session_image,"zhangyang","你好吗");
+        sessionList.add(b);
+        Session c=new Session(R.drawable.session_image,"chenxinyu","你好吗");
+        sessionList.add(c);
+        Session d=new Session(R.drawable.session_image,"moahanrong","你好吗");
+        sessionList.add(d);
+        Session e=new Session(R.drawable.session_image,"yangtiancheng","你好吗");
+        sessionList.add(e);
     }
 }
